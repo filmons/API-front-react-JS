4bc923f0f14320154614f41d4b8222bccd27d1b3
filmons <filmonseare@gmail.com>
2021-06-10 15:07:20 +0200

    the structure of the MVC front-end, in reactjs

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Home extends React.Component{
+
+    render(){
+        return (
+    <div className="App">
+        <header className="App-header">
+         <h1> this is Home Pages</h1>
+        </header>
+    </div>
+           
+        )
+    }
+  }
+  
+  export default Home
